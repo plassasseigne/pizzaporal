68bc7e2018f43c380e070ccab856644fbd522483
Paul Lassasseigne <paul.lassasseigne@gmail.com>
2025-02-21 23:32:25 +0100

    feat(check-table): add checktable command

---
+++ commands/check-table.js
@@ -0,0 +1,51 @@
+const formattingText = require("../utils/formatting-text");
+const colors = require("colors");
+const parseReservations = require("../utils/parser");
+const convertDay = require("../utils/convert-day");
+
+function checkTable(program) {
+  program
+    .command("checktable", "Returns the details of a table with its reserved slots")
+    .argument("<table>", "The number of the table")
+    .action(({ logger, args }) => {
+      const reservations = parseReservations();
+      const table = args.table;
+      const results = [];
+
+      for (const group of reservations) {
+        for (const reservation of group.reservations) {
+          if (reservation.table.includes(table)) {
+            results.push(reservation);
+          }
+        }
+      }
+
+      let reservationsFound = "";
+      
+      for (const reservation of results) {
+        let [ day, time ] = reservation.dayTime.split(" ");
+        day = convertDay(day);
+
+        reservationsFound += colors.white(`- ${colors.green(day)} at ${colors.green(time)} for ${colors.green(reservation.name)}. \n`)
+      }
+
+      // TODO : sort in order the days of the week
+
+      if (results.length === 0) {
+        logger.error(
+          formattingText(
+            colors.red(`No reservations associated with the ${colors.bold("TABLE " + table)}.`) + "\n"
+          )
+        )
+      } else {
+        logger.info(
+          formattingText(
+            `The reserved slots for the ${colors.bold.yellow("TABLE " + table)} :` + "\n" +
+            reservationsFound
+          )
+        )
+      }
+    })
+} 
+
+module.exports = checkTable;
--- commands/index.js
+++ commands/index.js
@@ -2,10 +2,12 @@ const checkReservation = require("./check-reservation");
 const getPizza = require("./get-pizza");
 const addReservation = require("./add-reservation");
 const generateICal = require("./generate-ical");
+const checkTable = require("./check-table");
 
 module.exports = (program) => {
   checkReservation(program);
   getPizza(program);
   addReservation(program);
   generateICal(program);
+  checkTable(program);
 }
